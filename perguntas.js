68bc816809b52bd805c3edf06a9aae86bfff7202
Maluco0007kc <00001100343386sp@al.educacao.sp.gov.br>
2024-12-03 15:43:05 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Basquete',
+    'onde o stephen curry joga?',
+    'warriors'
+)
+
+criaCartao(
+    'História',
+    'Quando foi descoberto o brasil?',
+    '1500'
+)
+
+criaCartao(
+    'basquete',
+    'qual time lebrom joga?',
+    'lakers'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz gato em Inglês?',
+    'Cat'
+)
